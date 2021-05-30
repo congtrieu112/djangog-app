a022dd8112d494bba66074840cbbaa1d6b371f3a
congtrieu <trieudc@devblock.net>
2021-05-30 16:50:51 +0700

    first source

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestAPIView(APIView):
+
+    def get(self, request):
+        return Response('ok')
