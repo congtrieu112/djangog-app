c806e25e76122f9e6432e11d180f9d08429db54f
congtrieu <trieudc@devblock.net>
2021-06-02 09:20:25 +0700

    update source

--- custom_user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django_use_email_as_username.admin import BaseUserAdmin
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-admin.site.register(User, BaseUserAdmin)
